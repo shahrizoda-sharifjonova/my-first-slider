248baf412d63b9c8c3f31c9e3184983f73dd8f86
shahrizoda <shahrizodadeveloping@gmail.com>
2022-02-27 19:27:12 +0500

    initial

--- script.js
+++ script.js
@@ -5,9 +5,33 @@ const img = document.querySelectorAll('#imgs img');
 
 let idx = 0;
 function changeImage() {
-    imgs.style.transform = `translateX(-500px)`
+    if(idx > img.length -1){
+        idx=0;
+    }else if(idx < 0){
+        idx = img.length -1
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`
 }
-rightBtn.addEventListener('click', changeImage)
+rightBtn.addEventListener('click', function change(){
+    idx++
+    changeImage()
+})
+leftBtn.addEventListener('click', function change(){
+    idx--
+    changeImage()
+})
+
+let interval = setInterval(run , 2000);
+
+function run() {
+    idx++;
+    changeImage()
+}
+function resetInterval() {
+    clearInterval(interval);
+
+    interval = setInterval(run, 2000);
+}
 
 
 
